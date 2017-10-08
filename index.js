44f8d3875c4689ecdc12dd75a397bdcbe8ae6327
Philipp Adrian <hello@philippadrian.com>
2017-10-08 20:49:08 +0000

    print out git history

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-something to commit
-and another line to test.
+const Git = require('nodegit');
+
+Git.Repository.open('./')
+  .then((repo)=>repo.getMasterCommit())
+  .then(function(firstCommitOnMaster) {
+    // Create a new history event emitter.
+    var history = firstCommitOnMaster.history();
+
+    // Create a counter to only show up to 9 entries.
+    var count = 0;
+
+    // Listen for commit events from the history.
+    history.on("commit", function(commit) {
+      // Disregard commits past 9.
+      if (++count >= 9) {
+        return;
+      }
+
+      // Show the commit sha.
+      console.log("commit " + commit.sha());
+
+      // Store the author object.
+      var author = commit.author();
+
+      // Display author information.
+      console.log("Author:\t" + author.name() + " <" + author.email() + ">");
+
+      // Show the commit date.
+      console.log("Date:\t" + commit.date());
+
+      // Give some space and show the message.
+      console.log("\n    " + commit.message());
+    });
+
+    // Start emitting events.
+    history.start();
+  });
