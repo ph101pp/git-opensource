35e1ddc22feafafabe4be500db0233bdd757ee0b
Philipp Adrian <hello@philippadrian.com>
2017-10-08 21:52:18 +0000

    add test folder

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const Git = require('nodegit');
 
+
+///////////////////////////////////////////////////////////////////////////////
+// INPUT
+
+const fromBranch = 'master';
+const toBranch = 'test'
+
+///////////////////////////////////////////////////////////////////////////////
+
 Git.Repository.open('./')
-  .then((repo)=>repo.getMasterCommit())
+  .then((repo)=>repo.getBranchCommit(fromBranch))
   .then(function(firstCommitOnMaster) {
     // Create a new history event emitter.
     var history = firstCommitOnMaster.history();
@@ -11,25 +20,42 @@ Git.Repository.open('./')
 
     // Listen for commit events from the history.
     history.on("commit", function(commit) {
-      // Disregard commits past 9.
-      if (++count >= 9) {
-        return;
-      }
-
-      // Show the commit sha.
-      console.log("commit " + commit.sha());
-
-      // Store the author object.
-      var author = commit.author();
-
-      // Display author information.
-      console.log("Author:\t" + author.name() + " <" + author.email() + ">");
-
-      // Show the commit date.
-      console.log("Date:\t" + commit.date());
-
-      // Give some space and show the message.
-      console.log("\n    " + commit.message());
+      
+      commit.getDiff()
+        .then((diff)=>diff[0].patches())
+        .then((patches)=>{
+          
+          
+          // Show the commit sha.
+          console.log("commit " + commit.sha());
+    
+          // Store the author object.
+          var author = commit.author();
+    
+          // Display author information.
+          console.log("Author:\t" + author.name() + " <" + author.email() + ">");
+    
+          // Show the commit date.
+          console.log("Date:\t" + commit.date());
+    
+          // Give some space and show the message.
+          console.log("\n    " + commit.message());  
+          
+          patches.forEach((patch)=>console.log(patch.newFile().path()));
+          
+          return patches[0].hunks().then((hunks)=>{
+            console.log('header', hunks[0].header());
+            console.log('header length', hunks[0].headerLen());
+            console.log('new lines', hunks[0].newLines());
+            console.log('new start', hunks[0].newStart());
+            console.log('old lines', hunks[0].oldLines());
+            console.log('old start', hunks[0].oldStart());
+          })
+          
+        });
+        
+        
+  
     });
 
     // Start emitting events.
