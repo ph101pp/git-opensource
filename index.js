84f52dc4bb9541d4dd7d7a89226d85cfa1b051cc
Philipp Adrian <philipp.adrian@dowjones.com>
2017-10-10 12:18:58 -0400

    create save patches folder in process.cwd

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const spawn = require('cross-spawn');
 const split = require('split2');
 const fs = require('fs');
 const ProgressBar = require('progress');
+const mkdirp = require('mkdirp');
 
-const PATH = './patches';
+const patchesPATH = './patches';
+const processPATH = process.cwd();
+const PATH = `${processPATH}/${patchesPATH}`;
+mkdirp.sync(PATH);
+
 
 const gitRevList = spawn.sync('git', ['rev-list', '--count', 'HEAD' ]);
+const commitCount = parseInt(gitRevList.stdout.toString());
+console.log('git-opensource analyzing:', commitCount, 'commits');
 const gitLog = spawn('git', ['log', '-p', '--format=email', '--full-diff', '--stat']);
 
 let fileCounter = 0;
 let fileStream = null;
 
-const progress = new ProgressBar('git-opensource [:bar] :current/:total :percent :etas', { 
-  total: parseInt(gitRevList.stdout.toString())
+const progress = new ProgressBar('git-opensource rewrite history: [:bar] :current/:total :percent', { 
+  total: commitCount
 })
 
 gitLog.stdout
@@ -22,8 +29,8 @@ gitLog.stdout
     const line = data.toString();
     
     if(line.indexOf('From ') === 0) {
-      const fileName = ('00000'+fileCounter++).split(-5);
-      fileStream = fs.createWriteStream(`${__dirname}/${PATH}/${fileName}.patch`);
+      const fileName = ('0000000'+fileCounter++).substr(-8);
+      fileStream = fs.createWriteStream(`${PATH}/${fileName}.patch`);
       progress.tick();
     }
     
