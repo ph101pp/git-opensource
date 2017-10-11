41e4d6a165ad41fc20e725e337b40af3a02949d4
Philipp Adrian <philipp.adrian@dowjones.com>
2017-10-10 22:42:44 -0400

    add git reset

--- git-opensource.sh
+++ git-opensource.sh
@@ -1,2 +1,9 @@
 #! /usr/bin/env bash
-git filter-branch --tree-filter 'git format-patch -1 --stdout $GIT_COMMIT|sed "s/^\+(?!\+\+\s(a|b|\/)).*$/+/"|git apply' -f
+git filter-branch --tree-filter '
+
+  c = `git format-patch -1 --stdout $GIT_COMMIT|sed "s/^\+(?!\+\+\s(a|b|\/)).*$/+/"`;
+  
+  git reset $GIT_COMMIT;
+  
+  c|git apply --cache' -f
+
