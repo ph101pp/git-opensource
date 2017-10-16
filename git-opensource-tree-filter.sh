5d6e5b304bbfe40197c929397c16a5b9f474d6f5
Philipp Adrian <philipp.adrian@dowjones.com>
2017-10-16 13:51:49 -0400

    use perl to replace additions

--- git-opensource-tree-filter.sh
+++ git-opensource-tree-filter.sh
@@ -3,10 +3,14 @@
 GIT_PARENT=`git log --pretty=%P -n 1 "$GIT_COMMIT"`;
 
 if [[ $GIT_PARENT != *" "* ]]; then
-  VAR=`git format-patch -1 --stdout $GIT_COMMIT`;
-  # VAR=`git format-patch -1 --stdout $GIT_COMMIT|sed "s/^\+(?!\+\+\s(a|b|\/)).*$/+/"`;
+  # PATCH=`git format-patch -1 --stdout $GIT_COMMIT`;
+  # PATCH=`git format-patch -1 --stdout $GIT_COMMIT|sed -e 's/^\+(?!\+\+\s(a|b|\/)).*$/\+/g'`;
+  PATCH=`git format-patch -1 --stdout $GIT_COMMIT|perl -ne "s/^\+(?!\+\+\s(a|b|\/)).*$/\+$GIT_COMMIT $GIT_AUTHOR_NAME/gm; print;"`;
+
+  echo "$PATCH";
 
   git reset $GIT_PARENT --hard;
+  git clean -f;
 
-  echo "$VAR" | git apply;
+  echo "$PATCH"|git apply --index;
 fi
