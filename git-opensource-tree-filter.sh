2cd235776545820c44db26b462460978754ed426
Philipp Adrian <philipp.adrian@dowjones.com>
2017-10-16 11:29:00 -0400

    rewrite history in place

--- git-opensource-tree-filter.sh
+++ git-opensource-tree-filter.sh
@@ -4,8 +4,9 @@ GIT_PARENT=`git log --pretty=%P -n 1 "$GIT_COMMIT"`;
 
 if [[ $GIT_PARENT != *" "* ]]; then
   VAR=`git format-patch -1 --stdout $GIT_COMMIT`;
+  # VAR=`git format-patch -1 --stdout $GIT_COMMIT|sed "s/^\+(?!\+\+\s(a|b|\/)).*$/+/"`;
 
   git reset $GIT_PARENT --hard;
 
-  echo "$VAR" | git apply --cache;
+  echo "$VAR" | git apply;
 fi
