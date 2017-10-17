38579fa1aab8713277978b9d4090c038b0d7bf6b
Philipp Adrian <philipp.adrian@dowjones.com>
2017-10-16 21:11:21 -0400

    git rm when parent empty

--- git-opensource-tree-filter.sh
+++ git-opensource-tree-filter.sh
@@ -2,15 +2,27 @@
 
 GIT_PARENT=`git log --pretty=%P -n 1 "$GIT_COMMIT"`;
 
+
 if [[ $GIT_PARENT != *" "* ]]; then
+
   # PATCH=`git format-patch -1 --stdout $GIT_COMMIT`;
   # PATCH=`git format-patch -1 --stdout $GIT_COMMIT|sed -e 's/^\+(?!\+\+\s(a|b|\/)).*$/\+/g'`;
-  PATCH=`git format-patch -1 --stdout $GIT_COMMIT|perl -ne "s/^\+(?!\+\+\s(a|b|\/)).*$/\+$GIT_COMMIT $GIT_AUTHOR_NAME/gm; print;"`;
+  PATCH=`git format-patch -1 --stdout $GIT_COMMIT|perl -ne "s/^\+(?!\+\+\s(a|b|\/)).*$/\+/gm; print;"|perl -ne "s/^\-(?!\-\-\s(a|b|\/)).*$/\-/gm; print;"`;
+  # PATCH=`git diff $GIT_PARENT...$GIT_COMMIT|perl -ne "s/^\+(?!\+\+\s(a|b|\/)).*$/\+/gm; print;"|perl -ne "s/^\-(?!\-\-\s(a|b|\/)).*$/\-/gm; print;"`;
 
   echo "$PATCH";
+  if [[ $GIT_PARENT == "" ]]; then  
+    git rm -rf "./";
+  else 
+    git reset $GIT_PARENT --hard;
+  fi 
 
-  git reset $GIT_PARENT --hard;
-  git clean -f;
+  # git clean -f;
 
-  echo "$PATCH"|git apply --index;
+  echo "$PATCH"|git apply;
+
+  git update-index --refresh;
+
+else 
+  echo "Parent $GIT_PARENT";
 fi
