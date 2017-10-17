c90a32a8d2d3712fc0685058f3c40a2fd66aed22
Philipp Adrian <philipp.adrian@dowjones.com>
2017-10-17 11:49:02 -0400

    silence most commands

--- git-opensource-tree-filter.sh
+++ git-opensource-tree-filter.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 GIT_OLD_PARENT=`git log --pretty=%P -n 1 "$GIT_COMMIT"`;
-GIT_NEW_PARENT=`cat /tmp/log`;
+GIT_NEW_PARENT=`cat /tmp/log 2> /dev/null`;
 NEW_LINE_CONTENT=" ";
 
 # echo "Parents: $GIT_OLD_PARENT >>>>>> $GIT_NEW_PARENT";
@@ -10,17 +10,18 @@ if [[ $GIT_NEW_PARENT != *" "* ]]; then
 
   if [[ $GIT_OLD_PARENT == "" ]]; then  
     PATCH=`git format-patch -1 --stdout $GIT_COMMIT`;
-    git rm -rf "./";
+    git rm -r --force --quiet "./";
   else 
     # PATCH=`git format-patch -1 --stdout $GIT_NEW_PARENT..$GIT_COMMIT|perl -ne "s/^\+(?!\+\+\s(a|b|\/)).*$/\+/gm; print;"`;
     PATCH=`git diff --patch --minimal $GIT_NEW_PARENT..$GIT_COMMIT`;
-    git reset $GIT_NEW_PARENT --hard;
+    git reset $GIT_NEW_PARENT --hard --quiet;
   fi 
   # echo "$PATCH";
+  git clean --force --quiet;
 
   PATCHED=`echo "$PATCH"|perl -ne "s/^\+(?!\+\+\s(a|b|\/)).*$/\+$NEW_LINE_CONTENT/gm; print;"`;
 
-  echo "$PATCHED"|git apply --index;
+  echo "$PATCHED"|git apply --index --whitespace 'nowarn';
 
 else 
   echo "Parent $GIT_NEW_PARENT";
--- git-opensource.sh
+++ git-opensource.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-rm /tmp/log;
+rm /tmp/log 2> /dev/null;
 git filter-branch --index-filter '_git-opensource-tree-filter' --commit-filter 'git commit-tree "$@" | tee /tmp/log' -f
